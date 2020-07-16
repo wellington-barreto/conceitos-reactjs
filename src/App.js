f16f789c1c4d4986b4de1fe978db56ae6e12a92d
Wellington Barreto <wellingtonbrt@gmail.com>
2020-07-16 03:15:29 -0300

    Conceitos ReactJS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  const loadRepositories = async () => {
+    const { data } = await api.get("/repositories");
+    setRepositories(data);
+  };
+
+
+  useEffect(() => {
+    loadRepositories();
+  } , []);
+
   async function handleAddRepository() {
-    // TODO
+     const response = await  api.post('repositories', {
+       title: `New Repository ${Date.now()}`,
+     url: "http://w3b.api",
+     techs: []
+    });
+
+
+  setRepositories([...repositories, response.data]);
+   
   }
 
+
+
   async function handleRemoveRepository(id) {
-    // TODO
+      await api.delete(`repositories/${id}`);
+      setRepositories(repositories.filter((repository) => repository.id !== id));
+
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+      {repositories.map(repository => 
+        <li key={repository.title}>
+         {repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+         )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
